fa2a7439bd3025e5ef5a0c8c875bb9914d556609
panos1998 <petridisp@ece.auth.gr>
2022-05-29 17:11:41 +0300

    history page added

---
+++ static/history.js
@@ -0,0 +1,22 @@
+document.getElementById('iy4ml').onclick = (event) => {
+    document.getElementById('history-container').style.setProperty('display','inline-block','important');
+    document.getElementById('hide-button').style.setProperty('display','inline-block','important');
+  };
+  document.getElementById('hide-button').onclick = (event) => {
+    document.getElementById('history-container').style.setProperty('display','none','important');
+  };
+  document.getElementById('izxo').onclick = (event) => {
+    event.preventDefault();
+    {
+      window.document.location = 'https://biomedicalapp.herokuapp.com/';
+    }
+  };
+  document.getElementById('itked').onclick = (event) => {
+    event.preventDefault();
+    {
+      window.document.location = 'https://biomedicalapp.herokuapp.com/assess';
+    }
+  };
+  window.onload = () => {
+  };
+  
