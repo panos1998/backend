8382695992a67c4346126bca7ff8cc608ff886b1
panos1998 <petridisp@ece.auth.gr>
2022-05-29 17:33:17 +0300

    history js

--- static/history.js
+++ static/history.js
@@ -1,7 +1,21 @@
 document.getElementById('iy4ml').onclick = (event) => {
     document.getElementById('history-container').style.setProperty('display','inline-block','important');
     document.getElementById('hide-button').style.setProperty('display','inline-block','important');
+    readlocalstorage();
   };
+  function readlocalstorage(){
+    var retrievedata= JSON.parse(localStorage.getItem('records'))
+    if (retrievedata === null){
+      console.log("empty array")}
+    else if(!Array.isArray(retrievedata)){
+      console.log(retrievedata)}
+    else {
+      retrievedata.forEach(element => {
+        console.log(element)
+      }
+                          );
+    }
+  }
   document.getElementById('hide-button').onclick = (event) => {
     document.getElementById('history-container').style.setProperty('display','none','important');
   };
@@ -15,6 +29,12 @@ document.getElementById('iy4ml').onclick = (event) => {
     event.preventDefault();
     {
       window.document.location = 'https://biomedicalapp.herokuapp.com/assess';
+    }
+  };
+  document.getElementById('i5z9m').onclick = (event) => {
+    event.preventDefault();
+    {
+      window.document.location = 'https://biomedicalapp.herokuapp.com/history/';
     }
   };
   window.onload = () => {
